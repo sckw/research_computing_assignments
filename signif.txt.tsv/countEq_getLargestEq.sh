b39cbe1ea4391b58d025da50a252c20404645016
sckw <scw2148@columbia.edu>
2017-09-12 21:04:44 -0400

    improved shell script

--- signif.txt.tsv/countEq_getLargestEq.sh
+++ signif.txt.tsv/countEq_getLargestEq.sh
@@ -1,3 +1,3 @@
 echo Total number of earthquakes in "$1": $(cut -f 3 Year_Mag_Country.tsv | sort | uniq -c | grep -w -i "$1")
-
 echo Largest earthquake magnitude in "$1": $(cut -f 2,3 Year_Mag_Country.tsv | grep -w -i "$1" | sort -n -r | head -n 1 | cut -f 1)
+echo Year of largest earthquake in "$1": $(cut -f 1,2,3 Year_Mag_Country.tsv | grep -w -i "$1" | sort -n -r | head -n 1 | cut -f 1)
