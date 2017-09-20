5ef53fe911cabfccdcea4683b7a9a9dea894a8b7
sckw <scw2148@columbia.edu>
2017-09-20 12:14:36 -0400

    Moving files into new directory

--- assignment_2/signif.txt.tsv/year.sh
+++ assignment_2/signif.txt.tsv/year.sh
@@ -3,4 +3,5 @@
 for YEAR in $(cut -f 1 Year_Mag_Country.tsv | uniq -c | sort -nr | head | cut -b 9-12)
 do
 	grep -w "$YEAR" Year_Mag_Country.tsv > $YEAR-earthquakes.txt
+	echo $YEAR-earthquakes.txt
 done
