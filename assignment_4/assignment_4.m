4b2405c78892eb1283b5b8f4ebbdc847e5431a94
sckw <scw2148@columbia.edu>
2017-09-26 20:39:07 -0400

    HW submission

---
+++ assignment_4/assignment_4.m
@@ -0,0 +1,285 @@
+%% Assignment 4: Plotting and analyzing a magnetic field induction time series
+%
+% - Answer the questions below by inserting the MATLAB commands you used into
+%   the blank space below each question.
+%
+% - When a question asks for a specific value (e.g., what is the length of vector x)
+%   list your answer as a comment by inserting it after a % symbol to the right of
+%   the command you used to get the answer. For example:
+%   length(x)    %  returns the value 12
+%
+%   This way, we should be able to run your assignment_4.m script without
+%   any errors being returned.
+%
+%  - For each figure created, save it as a PDF file.
+%
+% Turn in your assignment by uploading it to GitHub into a directory named
+% assignment_4. Make sure you upload the THREE figure PDF files in addition to assignment_4.m
+%
+%
+
+% 0. Its usually a good idea to start your scripts with a clean workspace,
+% so:
+
+    clear all;
+
+%% 1. In this section you will load the data file and examine what it contains.
+
+% 1a. Load the data file (list the command):
+Okmok_s00= load('Okmok_s00.mat');
+
+% 1b. What is the name of the variable that was loaded?
+who     % Your variables are: data
+whos    %Name      Size               Bytes  Class     Attributes
+        %data      1x1             44239168  struct         
+
+% 1c. What class type is it?
+whos % returns struct as the Class
+
+% 1d. How many memory bytes does it occupy?
+whos % data occupies 44239168 bytes
+
+% 1e. What is the name of the station where this data was collected?
+O = Okmok_s00.st;           % data.st shows all the fields in struct
+O.stationName % ans = 's00'
+
+% 1f. What is the magnetic field component measured and what are the units of the data?
+O.component   % ans = 'North'
+O.units       % ans = 'T' i.e. the units are in Tesla
+
+% 1g. What island was this data collected on?
+coordinates = sprintf('%g%c N %g%c W',O.latitude,char(176),O.longitude,char(176)) % coordinates = '53.3834° N -167.904° W'
+% ANSWER: This is Umnak Island.
+
+% 1h. What is the sampling rate of the time series?
+samplingRate = O.samplingFrequency % sampling rate = 128
+
+% 1i. What specific variable is the time series stored in?
+O.data; % stored in data
+
+% 1j. What is the start date and time of the time series? Hint: the
+% variable holding the start time uses MATLAB's datenum format.
+% Use the command that will convert it to a nicely formatted date and time string.
+datestr(O.timeDataStart) % ans = '04-Jul-2015 05:40:00'
+
+%% 2. In this section you will plot the data and add some labels and a title:
+
+
+% 2a. How many minutes long is the time series?
+minutesLength = ( length(O.data) / samplingRate ) / 60 % 720 minutes
+
+% 2b. Create a vector called minutes that gives the time in units of minutes
+% for each sample. The first sample should be at 0.0 minutes.
+i = linspace( 0 , length(O.data)-1 , length(O.data) );
+minutes =  i.*(720 / length(O.data)) ;
+
+% 2c. Plot the magnetic field time series vector versus the minutes vector
+% as a blue line. Add labels on the x and y axes for the
+% correct units.
+figure(1)
+plot(minutes,O.data*(10^9),'b-');
+xlabel('Time (minutes)');
+ylabel('Magnetic Field (nT)');
+
+% 2d. Add an informative title to the plot. You can create the string
+% using bracket notation:
+%
+% titleString = ['Station: ' <insert variable> ', Date and Time: ' <insert variable>];
+%
+% Replace each <insert variable> with the correct variables for the station
+% name and the start date and time. Then use this string to make a title on
+% the plot.
+titleString = ['Station: ' O.stationName ', Date and Time: ' datestr(O.timeDataStart)];
+title({'Magnetic field time series taken at:'; titleString});
+
+%2e. Save the figure as the file 'timeseries.pdf'. List the command you
+%    used below.
+saveas(1,'timeseries','pdf')
+
+%% 3. In this section you will compute the power spectral density (PSD) of the time series.
+% The PSD is often just referred to as the spectrum of the time series. It
+% shows the power present in the time series as a function of frequency.
+% If you have already taken QMDA, you have already learned about
+% power spectra. If not, have a quick read of the wikipedia page for
+% Spectral Density.
+%
+
+% 3a. How many samples are in 300 seconds of the data?
+samples300 = samplingRate*300 % samples300 = 38400 samples in 300 seconds of the data.
+
+% 3b. Make a vector called first that has the first five minutes of data and another
+% vector called last which has the last five minutes of data:
+first = O.data( 1 : samples300 );
+last = O.data( end-samples300+1: end );
+
+% 3c. Create a new time vector called seconds that has time in seconds for
+% the first and last vectors. The starting value should be zero. This should
+% be similar to what you did in exercise 2b but now for seconds rather than
+% minutes.
+j = linspace( 0 , (samples300 - 1), samples300 );
+seconds = [ j.*(minutesLength*60 / length(O.data)) ];
+
+% 3d. Make a new figure with a subplot grid containing 2 rows x 1 column.
+% In the top subplot, plot both the first and last vectors versus the seconds
+% vector. Plot first as a blue line and last as a red line. Make sure you label the x
+% and y axes with the proper units. Add a legend with the labels 'first 5 minutes'
+% and 'last 5 minutes'.
+figure(2);
+ax1=subplot(2,1,1);
+ax2=subplot(2,1,2);
+plot(ax1,seconds, first*(10^9), 'b-',seconds, last*(10^9),'r-');
+xlabel(ax1,'Time (minutes)')
+ylabel(ax1,'Magnetic field (nT)')
+legend(ax1,'first 5 minutes','last 5 minutes','Location','southeast')
+
+
+% 3e. Compute the power spectrum of the first and last
+% time series vectors. To do this, use the function
+% pspectrum.m (which you downloaded above) to compute the PSD
+[psd_first, frequencies] = pspectrum(first,samplingRate);
+[psd_last, frequencies] = pspectrum(last,samplingRate);
+
+% 3f. In the 2nd subplot, plot both power spectra using a log scale for
+% frequency on the x axis and a log scale for power on the y axis. Again
+% use a blue line for the first data and a red line for the last data. Add
+% labels for the x and y axis units. Note that the units of the PSD are T^2/Hz.
+hold on;
+plot(ax2,frequencies,psd_first,'b-',frequencies,psd_last,'r-');
+box
+set(ax2,'XScale','log')
+set(ax2,'YScale','log')
+xlabel(ax2,'frequencies (s^{-1})')
+ylabel(ax2,'Power spectral density (T^{2}Hz^{-1})')
+legend(ax2,'first 5 minutes','last 5 minutes','Location','northeast')
+
+% 3g. Save the figure as file 'first_last_psd.pdf'. But first use the
+% command 'orient tall' to tell MATLAB to make the figure fill the
+% entire printed page. List the commands you used below.
+orient tall
+saveas(2,'first_last_psd','pdf');
+
+% 3h. The broad peaks (bumps) in the spectra around 7.8, 14.3, 20.8, ... Hz are
+% the Schumann resonances formed by the energy from lightning strikes
+% resonating in the non-conducting cavity between the electrically
+% conductive ground and conductive ionosphere about 90 km above. Large
+% lightning strikes will excite normal modes in this cavity as the energy
+% repeatedly travels around the Earth. Do the Shumann resonances for the
+% first and last five minutes of data look similar or different?
+
+%ANSWER: Yes, they do look similar.
+
+
+% 3g. What differences do the power spectra show at frequencies below
+% 1 Hz?  Does that agree with what the time series shows?
+
+%ANSWER: Below 1 Hz, the first and last five minutes are different on the
+%order of ~100 T^2/Hz. The last 5 minutes have a higher power spectral
+%density than the first 5 minutes, below 1 Hz.
+
+
+%% 4. Spectrogram. For many time series, the PSD is non-stationary,
+% meaning that it changes over time, like what we saw in exercise 3.
+% You can show the time evolution in a nice graphical form by creating a spectrogram,
+% which is basically a series of power spectra computed by chopping up
+% a time series into small sections and computing the PSD for each section.
+% The resulting matrix of PSD's can be shown as a 2D matrix or surface
+% plot as a function of time and frequency. In this exercise, you
+% will go through the steps to make a spectrogram.
+
+% 4a. We will use 300 second long sections of data for the spectrogram.
+% Create a variable called nSamplesPerSection which has the number of time
+% series points (i.e. samples) for each 300 s long data section.
+nSamplesPerSection = samplingRate*300; % returns 38400 (same as samples300)
+
+% 4b. For an arbitrary section i (where i is an integer), write the commands
+% that will give the starting and ending indices of the data for that section
+% in the time series. Call them iStart and iEnd.
+clear i
+i = 144;                                    % an arbitrary section
+if i < 1
+    disp('Invalid section. i = 1 is the first section.')
+elseif i > 144
+    disp('Invalid section. i exceeds data dimensions')
+else
+    iStart = 1 + (i-1)*nSamplesPerSection;  % returns iStart = 5491201
+    iEnd = (i)*nSamplesPerSection;          % returns iEnd = 5529600
+end
+
+% 4c. Given the value in nSamplesPerSection, create a variable called
+% nSections that has the total number of sections that will be created by
+% chopping up the time series in sections of 300 s length.
+nSections = length(O.data)/nSamplesPerSection % returns nSections = 144
+
+% Check your formulas for iStart and iEnd. If you assign i to be the last
+% section, does iEnd equal the length of st.data?
+i = nSections;                        % nSections = 144 is the last section
+if iEnd == length(O.data)             % iEnd is the same length as O.data
+disp('iEnd equals the length of O.data')
+else
+disp('Error with formulas')
+end
+
+% 4d. Now its time to put all the pieces together to compute the
+% spectrogram. Create a for loop that will compute the PSD for each data section.
+% The resulting spectra should be stored in a matrix call PSD_matrix.
+% PSD_matrix will have length(f) rows and nSections columns.
+%
+% Hints: create a for loop over nSections, use iStart and iEnd to
+% extract that section of data from st.data, and then compute the power spectrum of
+% that section. Insert the resulting spectrum into a column in PSD_matrix.
+%
+% This will be somewhat computationally intensive. It took a minute to run
+% on my laptop. If your for loop variable is i, insert  "disp(i);" inside the
+% for loop so you can track its progress while its running.
+PSD_matrix = [];
+for k = [1:1:nSections]
+    iStart = 1 + (k-1)*nSamplesPerSection;
+    iEnd = (k)*nSamplesPerSection;
+    section = [O.data(iStart:iEnd)];
+    PSD_matrix(:,k) = pspectrum(section,samplingRate);
+    disp(k);
+end                 % it took about two minutes for this to run on my laptop.
+
+%%
+% 4e. Create a new figure and plot the spectrogram using the pcolor command.
+% The y axis should be frequency on a log10 scale and the x axis should
+% be time in minutes (you will need to create a time vector that represents
+% the 5 minute spacing between data sections).
+
+% Additional instructions:
+% - Since the power spectra cover a large dynamic range, use log10(PSD_matrix) inside the pcolor command.
+% - Use the "shading" command with one of its options to improve the appearance of the plot
+% - Add unit labels to the x and y axis
+% - Add a title using the results from exercise 2d.
+% - Add a colorbar
+% - Experiment with the color limits by using the "caxis" command.
+
+time = [zeros(nSections,1)];
+for k = [1:1:nSections]
+    iStart = 1 + (k-1)*nSamplesPerSection;
+    time(k,:) = [minutes(iStart)];
+end
+
+figure(3);
+h = pcolor(time,frequencies,log10(PSD_matrix));
+set(gca,'YScale','log');
+H = colorbar;
+colormap(jet)
+set(gca,'YLim',[0 0.2]);
+set(h,'LineStyle','None');
+set(gca,'Layer','top');
+shading(gca,'interp');
+caxis([-24 -16])
+xlabel('Time (minutes)');
+ylabel('Frequency (Hz)');
+H.Label.String = 'log_{10}(Power spectral density) (T^{2}Hz^{-1})';
+set(H,'FontSize',12);
+set(gca,'FontSize',12);
+titleString = ['Station: ' O.stationName ', Date and Time: ' datestr(O.timeDataStart)];
+title(titleString);
+
+% 4f.  Save the figure as file 'spectrogram.pdf'.
+saveas(3,'spectrogram','pdf');
+
+
+
