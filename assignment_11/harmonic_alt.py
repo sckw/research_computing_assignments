aa4548e38331c7109764510f0efd4141502dc195
Suki Wong <wong@ldeo.columbia.edu>
2017-11-29 21:16:54 -0500

    corrections made

--- assignment_11/harmonic_alt.py
+++ assignment_11/harmonic_alt.py
@@ -4,25 +4,24 @@ import numpy as np
 
 def alternating_harmonic_series(N,rank,size):
     val = 0.
-    for n in 1,int(N/size):
-        i=n*size+rank
-        val += (-1)**(i+1) / i 
+    for n in range(rank+1,N,size):
+        val += (-1)**(n+1) / n 
     return val
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-
 start = timer()
 N = 100000000
-global_sum = np.array(0.0,'d')
 value = alternating_harmonic_series(N,rank,size)
-send_val = np.array(value,'d')
-comm.Reduce(send_val, global_sum, op=MPI.SUM,root=0)
 stop  = timer()
+
+value_sum = np.array(0.0,'d')
+send_val = np.array(value,'d')
+comm.Reduce(send_val, value_sum, op=MPI.SUM,root=0)
 
 if rank == 0:
     print(' Number of terms in series: ',N)
-    print(' Ending value of series: ',value)
+    print(' Ending value of series: ',value_sum)
     print(' Elapsed time: ',stop-start)
